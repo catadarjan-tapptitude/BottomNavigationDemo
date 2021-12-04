cc208cd0958c645b4291f34f89013cf0fe3afc36
Catalin <catalin.darjan@tapptitude.com>
2021-12-04 08:37:40 +0200

    Fix image quality issues Use ScrollView within DetailsFragment

--- app/src/main/java/tapp/bottomnav/demo/data/ArticlesData.kt
+++ app/src/main/java/tapp/bottomnav/demo/data/ArticlesData.kt
@@ -9,32 +9,32 @@ val articlePreviews = listOf(
         Date(),
         "Lorem Ipsum 1",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex",
-        "https://picsum.photos/id/1/100/100"
+        "https://picsum.photos/id/1/300/300"
     ),
     ArticlePreview(
         2,
         Date(),
         "Lorem Ipsum 2",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex",
-        "https://picsum.photos/id/2/100/100"
+        "https://picsum.photos/id/2/300/300"
     ),
     ArticlePreview(
         3,
         Date(),
         "Lorem Ipsum 3",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex",
-        "https://picsum.photos/id/3/100/100"
+        "https://picsum.photos/id/3/300/300"
     ), ArticlePreview(
         4,
         Date(),
         "Lorem Ipsum 4",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex",
-        "https://picsum.photos/id/4/100/100"
+        "https://picsum.photos/id/4/300/300"
     ), ArticlePreview(
         5,
         Date(),
         "Lorem Ipsum 5",
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex",
-        "https://picsum.photos/id/5/100/100"
+        "https://picsum.photos/id/5/300/300"
     )
 )
--- app/src/main/java/tapp/bottomnav/demo/data/DestinationsData.kt
+++ app/src/main/java/tapp/bottomnav/demo/data/DestinationsData.kt
@@ -6,31 +6,31 @@ import tapp.bottomnav.demo.feature.travel.adapter.Destination
 val destinations = listOf(
     Destination(
         1,
-        "https://picsum.photos/id/43/400/400",
+        "https://picsum.photos/id/43/800/800",
         "New York",
         Date()
     ),
     Destination(
         2,
-        "https://picsum.photos/id/44/400/400",
+        "https://picsum.photos/id/44/800/800",
         "Boston",
         Date()
     ),
     Destination(
         3,
-        "https://picsum.photos/id/45/400/400",
+        "https://picsum.photos/id/45/800/800",
         "Los Angeles",
         Date()
     ),
     Destination(
         4,
-        "https://picsum.photos/id/46/400/400",
+        "https://picsum.photos/id/46/800/800",
         "Bucharest",
         Date()
     ),
     Destination(
         5,
-        "https://picsum.photos/id/47/400/400",
+        "https://picsum.photos/id/47/800/800",
         "London",
         Date()
     )
